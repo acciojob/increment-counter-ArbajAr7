b25e6a9ba652f112b99fd26f901fdb67b5069c8c
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-10 15:42:19 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var number = 1;
+function incrementNumber() {
+	number += 1;
+  document.getElementById("abc").innerHTML = number;
+}
+document.getElementById('xyz').innerHTML += "<p id='abc'>" + number + "</p>";
+document.getElementById('xyz').innerHTML += "<button id ='btn'>Increment</button>";
+document.getElementById('btn').addEventListener("click",incrementNumber);
