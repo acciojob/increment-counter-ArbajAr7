d5aba10f2199cbbbb90064351a20d218e81916f0
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-10 15:56:15 +0545

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function incrementNumber() {
 	number += 1;
   document.getElementById("counter").innerHTML = number;
 }
-document.getElementById('xyz').innerHTML += "<p id='counter'>" + number + "</p>";
+document.getElementById('xyz').innerHTML += "<p id='counter'>1</p>";
 document.getElementById('xyz').innerHTML += "<button id ='incrementBtn'>Increment</button>";
 document.getElementById('incrementBtn').addEventListener("click",incrementNumber);
